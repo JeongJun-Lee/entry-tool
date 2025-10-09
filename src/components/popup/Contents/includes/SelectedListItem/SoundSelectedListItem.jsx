a3a0d5f3a9a3cda0ed622dac10834186b50329d0
JJ Lee <comseong@gmail.com>
2025-10-09 09:19:10 +0500

    Fix a issue not to show sound name by current lang

--- src/components/popup/Contents/includes/SelectedListItem/SoundSelectedListItem.jsx
+++ src/components/popup/Contents/includes/SelectedListItem/SoundSelectedListItem.jsx
@@ -1,12 +1,16 @@
 import { CommonUtils } from '@utils/Common';
 
-export default ({ theme, item, onDelete }) => (
-    <li>
-        <div className={theme.thmb}>
-            <span className={theme.text_box}>{item.name}</span>
-        </div>
-        <a className={theme.btn_del} onClick={onDelete}>
-            <span className={theme.blind}>{CommonUtils.getLang('Buttons.delete')}</span>
+export default ({ theme, item, onDelete }) => {
+    const { name } = CommonUtils.getImageSummary(item);
+
+    return (
+        <li>
+            <div className={theme.thmb}>
+                <span className={theme.text_box}>{name}</span>
+            </div>
+            <a className={theme.btn_del} onClick={onDelete}>
+                <span className={theme.blind}>{CommonUtils.getLang('Buttons.delete')}</span>
         </a>
     </li>
-);
+    );
+}
