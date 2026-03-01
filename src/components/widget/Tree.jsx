cdc0fd5055a7424ea0495c8b3ae9a58de7cb5dc2
JJ Lee <comseong@gmail.com>
2026-03-01 14:40:10 +0500

    refactor: Restructure tree rendering to use `div` for root, enhance prediction logic, and refine `drawTree` HTML output.

--- src/components/widget/Tree.jsx
+++ src/components/widget/Tree.jsx
@@ -23,7 +23,7 @@ const Tree = (props) => {
                     <div className={theme.body}>
                         <div className={theme.tree_container}>
                             <div className={theme.tree}>
-                                <ul dangerouslySetInnerHTML={{ __html: treeHtml }} />
+                                <div dangerouslySetInnerHTML={{ __html: treeHtml }} />
                             </div>
                         </div>
                     </div>
@@ -35,7 +35,7 @@ const Tree = (props) => {
 
 export default Tree;
 
-const drawTree = (source, labelPrefix = '') => {
+const drawTree = (source) => {
     const { graphData, fields, valueMap, yes = 'yes', no = 'no' } = source;
     const { splitColumn, splitValue, distribution, left, right } = graphData;
     let feature = null;
@@ -45,14 +45,23 @@ const drawTree = (source, labelPrefix = '') => {
     } else if (splitColumn !== null && splitColumn !== undefined) {
         feature = fields[splitColumn + 1] || fields[splitColumn];
     }
-    if (graphData.predictionLabel !== undefined) {
+    if (graphData.predictionLabel !== undefined && graphData.predictionLabel !== null) {
         prediction = graphData.predictionLabel;
     } else if (distribution !== null && distribution !== undefined) {
         let d = distribution.data ? distribution.data : distribution;
         while (Array.isArray(d) && d.length === 1) {
             d = d[0];
         }
-        if (typeof d === 'object' && d !== null) {
+        if (Array.isArray(d)) {
+            if (d.length > 0) {
+                const sorted = [...d].sort((a, b) => b - a);
+                const maxRowIndex = d.indexOf(sorted[0]);
+                prediction = valueMap[maxRowIndex + 1] !== undefined ? valueMap[maxRowIndex + 1] : valueMap[maxRowIndex];
+                if (prediction === undefined || prediction === null) {
+                    prediction = maxRowIndex;
+                }
+            }
+        } else if (typeof d === 'object' && d !== null) {
             let bestClass = -1;
             let maxProb = -Infinity;
             for (const [k, v] of Object.entries(d)) {
@@ -62,33 +71,33 @@ const drawTree = (source, labelPrefix = '') => {
                 }
             }
             if (bestClass !== -1) {
-                let lbl = valueMap[bestClass + 1] !== undefined ? valueMap[bestClass + 1] : valueMap[bestClass];
-                if (lbl !== undefined && lbl !== null && lbl !== '') {
-                    prediction = lbl;
+                prediction = valueMap[bestClass + 1] !== undefined ? valueMap[bestClass + 1] : valueMap[bestClass];
+                if (prediction === undefined || prediction === null) {
+                    prediction = bestClass;
                 }
             }
+        } else if (typeof d !== 'object') {
+            prediction = valueMap[Number(d) + 1] || valueMap[d] || d;
         }
     }
-
-    const isLeafNode = !left && !right || (Object.keys(left || {}).length === 0 && Object.keys(right || {}).length === 0);
-    let labelHtml = labelPrefix ? `<div class="label-box">${labelPrefix}</div>` : '';
-
-    if (isLeafNode) {
-        const predLabel = prediction !== undefined && prediction !== null ? prediction : 'Leaf';
+    // leaf
+    if (!left && !right) {
+        const leafLabel = (prediction !== undefined && prediction !== null && prediction !== '') ? prediction : 'Leaf';
         return [
+            '<ul>',
             '<li>',
-            labelHtml,
             '<a href="#">',
             '<b>',
-            predLabel,
+            leafLabel,
             '</b>',
             '</a>',
             '</li>',
+            '</ul>',
         ].join('');
     }
     return [
+        '<ul>',
         '<li>',
-        labelHtml,
         '<a href="#">',
         '<b>',
         feature,
@@ -97,9 +106,16 @@ const drawTree = (source, labelPrefix = '') => {
         '</b>',
         '</a>',
         '<ul>',
-        drawTree({ graphData: left, fields, valueMap, yes, no }, yes),
-        drawTree({ graphData: right, fields, valueMap, yes, no }, no),
+        '<li>',
+        `<a href="#">${yes}</a>`,
+        drawTree({ graphData: left, fields, valueMap, yes, no }),
+        '</li>',
+        '<li>',
+        `<a href="#">${no}</a>`,
+        drawTree({ graphData: right, fields, valueMap, yes, no }),
+        '</li>',
         '</ul>',
         '</li>',
+        '</ul>',
     ].join('');
 };
