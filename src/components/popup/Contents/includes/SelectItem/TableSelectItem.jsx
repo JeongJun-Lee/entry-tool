dc6b144fb7e4b7e18fc6a51ecad34c6ce96dae3a
JJ Lee <comseong@gmail.com>
2026-03-12 22:23:14 +0500

    refact: Remove `useEffect` hook that reset item selection and option state.

--- src/components/popup/Contents/includes/SelectItem/TableSelectItem.jsx
+++ src/components/popup/Contents/includes/SelectItem/TableSelectItem.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef } from 'react';
 import { openModal } from '@actions/popup';
 import { connect } from 'react-redux';
 import { CommonUtils } from '@utils/Common';
@@ -39,13 +39,6 @@ const DetailModal = ({ theme, info = {} }) => {
 
 const TableSelectItem = ({ theme, item, upload, openModal, dropdownState }) => {
     const [option, setOption] = useState(0);
-
-    useEffect(() => {
-        // 팝업이 다시 열릴 때 이전 선택값이 남지 않도록 초기화합니다.
-        item.selected = item.projectTable;
-        delete item.selectedSubtype;
-        setOption(0);
-    }, [item._id || item.id]);
 
     const handleSelect = (option) => {
         const [, index] = option;
